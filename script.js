bdb271ccab14195d62fc8b7831437f16c9eb4504
MbiteX <89062974246@ya.ru>
2024-02-29 17:23:26 +0300

    apdate

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const box = urlParams.get('box')
+console.log(box);
+/* I put your JSON into an external file, loaded from github */
+const url = "/box/" + box + ".json";
+
+const nameBox = document.getElementById("box");
+nameBox.insertAdjacentHTML('beforeend', box)
+
+$(document).ready(function () {
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        error: function () {
+            console.log('JSON FAILED for data');
+        },
+        success: function (results) {
+
+
+            var cartItemsList = document.getElementById("contbox");
+            let raz = 0
+            let pre = 0
+            results.box.forEach(function (e) {
+                raz = e.ind - pre
+                pre = e.ind
+
+
+                cartItemsList.insertAdjacentHTML('beforeend', "<tr><td>" + e.date + "</td><td>" + e.ind + " (+" + raz + ") " + "</td><td>" + e.sum + "</td></tr>");
+            });
+        }
+    })
+})
